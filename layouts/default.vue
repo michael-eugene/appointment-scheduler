82ce792b680c64101786f339f80a94aacf2fcef3
michael-eugene <eugenemichael21@yahoo.com>
2024-11-08 14:09:39 -0500

    v1.1

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <script setup>
 const user = useSupabaseUser();
+const supabase = useSupabaseClient();
 const signOut = async () => {
   const { error } = await supabase.auth.signOut();
   if (error) console.log(error);
@@ -9,7 +10,7 @@ const signOut = async () => {
 <template>
   <div>
     <header class="flex justify-end px-4 py-2">
-      <button @click="signOut" v-show="user" class="">Sign Out</button>
+      <button @click="signOut" v-show="user">Sign Out</button>
     </header>
     <div class="md:flex gap-10 full mx-4 md:mx-16">
       <div class="w-full"><slot /></div>
--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,12 @@ definePageMeta({
 
 // supabase user param
 const user = useSupabaseUser();
+const supabase = useSupabaseClient();
 const signOut = async () => {
   const { error } = await supabase.auth.signOut();
   if (error) console.log(error);
   router.push("/login");
 };
-
-
-const supabase = useSupabaseClient();
 
 // vue-router registry
 const router = useRouter();
@@ -23,38 +21,41 @@ let { data: services, error } = await supabase.from("services").select();
 
 <template>
   <div>
-    <p class="text-sm">Welcome back, {{ user.name }}. Not you? <button @click="signOut" class="text-gray-500">Sign Out</button></p>
+    <p class="text-sm">
+      Welcome back, {{ user.name }}. Not you?
+      <button @click="signOut" class="text-gray-500">Sign Out</button>
+    </p>
 
     <ul>
       <NuxtLink to="date">
-      <li
-        v-for="service in services"
-        :key="service.id"
-        class="flex flex-row justify-between p-4 w-full items-center cursor-pointer my-2 border-2 border-gray-900 rounded-lg hover:border-emerald-500"
-      >
-        <div>
-          <p class="font-bold text-lg">{{ service.name }}</p>
-          <br />
-          <p class="text-gray-500 text-sm">{{ service.description }}</p>
-        </div>
-        <div class="flex">
-          <p class="font-bold">${{ service.price }}</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m8.25 4.5 7.5 7.5-7.5 7.5"
-            />
-          </svg>
-        </div>
-      </li>
+        <li
+          v-for="service in services"
+          :key="service.id"
+          class="flex flex-row justify-between p-4 w-full items-center cursor-pointer my-2 border-2 border-gray-900 rounded-lg hover:border-emerald-500"
+        >
+          <div>
+            <p class="font-bold text-lg">{{ service.name }}</p>
+            <br />
+            <p class="text-gray-500 text-sm">{{ service.description }}</p>
+          </div>
+          <div class="flex">
+            <p class="font-bold">${{ service.price }}</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m8.25 4.5 7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </div>
+        </li>
       </NuxtLink>
     </ul>
   </div>
